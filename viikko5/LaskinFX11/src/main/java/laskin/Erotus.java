5f0ca0b1120717a49bf4fbed093d5843747d7755
Amalia Ahola <amalia.ahola@gmail.com>
2021-04-22 14:40:19 +0300

    tehtava 4

---
+++ viikko5/LaskinFX11/src/main/java/laskin/Erotus.java
@@ -0,0 +1,25 @@
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Erotus extends Komento {	
+	
+	public Erotus(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+		super(tuloskentta, syotekentta, nollaa, undo, sovellus);
+	}
+	
+	@Override
+	public void suorita() {
+		sovellus.miinus(Integer.parseInt(syotekentta.getText()));
+		syotekentta.setText("");
+        tuloskentta.setText("" + sovellus.tulos());
+        onkoTulosNolla(sovellus.tulos());
+	}
+
+	@Override
+	public void peru() {
+	}
+	
+
+}
